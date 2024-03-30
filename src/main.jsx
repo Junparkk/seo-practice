d30c0d336f9bd121386fffacc52e13fb4dfac498
박효준 <hyojun_park@tmax.co.kr>
2024-03-31 08:49:55 +0900

    react-snap 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById("root");
+
+// 서버 사이드 렌더링을 위한 조건부 로직
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrateRoot(
+    rootElement,
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  // 클라이언트 사이드 렌더링 (CSR)
+  const root = ReactDOM.createRoot(rootElement); // React 18에서의 새로운 방식
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
