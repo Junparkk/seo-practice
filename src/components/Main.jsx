0f796169cbdc512dcd545ecb48ac19121618c61a
박효준 <hyojun_park@tmax.co.kr>
2024-04-01 20:51:29 +0900

    test

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,11 @@
 import { Helmet } from "react-helmet";
+
+import fav from "../assets/favicon-16x16.png";
+import favLg from "../assets/favicon-32x32.png";
+import apple from "../assets/apple-touch-icon.png";
+import android from "../assets/android-chrome-192x192.png";
+import androidLg from "../assets/android-chrome-512x512.png";
+
 function Main() {
   return (
     <>
@@ -34,6 +41,16 @@ function Main() {
           property="twitter:image"
           content="https://i.ytimg.com/vi/ax3g7UiFyk8/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLBw65Y9akK26AVzDxvEDAwJWC8sTA"
         />
+
+        <link rel="apple-touch-icon" sizes="180x180" href={apple} />
+        <link rel="icon" type="image/png" sizes="192x192" href={android} />
+        <link rel="icon" type="image/png" sizes="512x512" href={androidLg} />
+        <link rel="icon" type="image/png" sizes="32x32" href={favLg} />
+        <link rel="icon" type="image/png" sizes="16x16" href={fav} />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Helmet>
       <div>Main</div>
     </>
